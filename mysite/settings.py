252c842884c065388aa12b62f4c953ecf0991d84
PAwA1207 <skalolaz2000@gmail.com>
2025-02-04 20:29:25 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'main/static',
 ]
-STATIC_ROOT = '/root/mysite/staticfiles/'
+STATIC_ROOT = '/var/www/mysite/staticfiles/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
